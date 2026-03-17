e481093ae44654e8f9e37422ab83e00cbf00afe5
Brandon Monier <bm646@cornell.edu>
2026-03-17 13:51:54 -0500

    Initial commit

---
+++ src/001_variables/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int length = 2;
+    int width = 4;
+    int area = length * width;
+
+    printf("Area calculation: %d\n", area);
+    return 0;
+}
--- src/hello/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char *name = "Brandon";
-    printf("Hello, %s\n", name);
-    return(0);
-}
