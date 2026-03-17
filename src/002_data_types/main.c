041a93fdeca50bda658123970e990af2fc90359b
Brandon Monier <bm646@cornell.edu>
2026-03-17 14:00:10 -0500

    Initial commit

---
+++ src/002_data_types/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int studentID = 42;
+    float score = 1.23;
+    char grade = 'B';
+
+    printf("Student ID...: %d\n", studentID);
+    printf("Score........: %.2f\n", score);
+    printf("Grade........: %c\n", grade);
+    return 0;
+}
