19bf305d82b75a2ee38348160bb57a891dade0bd
Brandon Monier <bm646@cornell.edu>
2026-03-18 11:30:11 -0500

    Initial commit

--- src/011_breaks/main.c
+++ src/011_breaks/main.c
@@ -4,7 +4,7 @@ int main() {
     int myNumbers[] = {3, -1, 7, 0, 9};
     int length = sizeof(myNumbers) / sizeof(myNumbers[0]);
     int i;
-    
+
     for (i = 0; i < length; i++) {
       if (myNumbers[i] < 0) {
         continue; // skip negative numbers
