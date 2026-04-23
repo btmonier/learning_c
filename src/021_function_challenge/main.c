eb1f80e5b32d509f9bdfda620a9d9e1d3c23fa13
Brandon Monier <bm646@cornell.edu>
2026-04-23 17:07:55 -0500

    Add simpler macro

--- src/021_function_challenge/main.c
+++ src/021_function_challenge/main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 // Jumping ahead here...
-#define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
+#define ARRAY_LENGTH(x) (sizeof(x) / sizeof((x)[0]))
 
 int sum(int x[], int size);
-
 
 int main() {
     int obs[] = { 3, 4, 5, 1 };
     // int arr_len = sizeof(obs) / sizeof(obs[0]);
-    int arr_len = COUNT_OF(obs);
+    int arr_len = ARRAY_LENGTH(obs);
 
     int res = sum(obs, arr_len);
 
     printf("sum() res: %d\n", res);
     return 0;
 }
-
 
 int sum(int x[], int size) {
     int res = 0;
